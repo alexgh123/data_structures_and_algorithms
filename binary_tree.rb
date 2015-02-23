987830983d3d992742eb5c45d713f7d35867ee34
Alex Hardt <hardt.alex@gmail.com>
2015-02-23 10:28:07 -0600

    typing out thoughts on how to create node objects with the children

--- binary_tree.rb
+++ binary_tree.rb
@@ -20,45 +20,27 @@ class Node
      #child node always have a parent
      #some nodes don't have children
 
+     #the original midpoint we describe as a node object, but after that, we are kinda recrusively turning values into node objects
+
+     #the midpoint is always a node object (this will get me a balanced tree), then i find the new midpoint of the two children and create new node objects.
+
+     #if the left or right child is an array length of 1, it is a node object, but without children
+
+     #i also have to describe if i want my node tree balanced or not
+
+
   end
 end
 
     def build_tree(array)
-      #goes to mid point of array (because its sorted)
-      #assigns midpoint object to the middle number
-      #then it goes through numberes to the left and right and assigns them as children (left or right) and assigns a parent
-      array = array.sort #this just confirms my array is sorted, technically I won't need this later on once my algorithm works
-      #adding line for more descriptive commit
+      array = array.sort #im supposed to assume its sorted
       length_of_array = array.length
       midpoint = array[(length_of_array/2)]
       index_of_midpoint = array.index(midpoint)
-      #what if my midpoint is duplicated occurs twice, does my logic cover that
       left_child = array[0..(index_of_midpoint-1)]
       right_child = array[(index_of_midpoint+1)..-1]
-      p "array is:"
-      p array
-      p "midpoint is: #{midpoint}"
-      p "left child is: #{left_child}"
-      p "right_child is: #{right_child}"
-
-      #i think i'm just continually assigning these variables until i can't do it any more
-
-      #how will i know when im done? doesn't this data need to be persistant?
-
-      #so this build tree might make more sense outside of the node class
-
-      # once there is only one number, make that bottom number a node.
-
-      #the first thing to do is make the midpoint a node
-
-      p "----------"
       original_node = Node.new(midpoint, left_child, right_child)
-      p "hey the OG original node is: #{original_node}"
-      p "it's value is: #{original_node.value}"
-      p "its left children are: #{original_node.left_child}"
-      p "its right children are: #{original_node.right_child}"
-
-
+      #where do my nodes go? an array?
     end
 
      def crawler
@@ -68,7 +50,7 @@ end
 
 
 build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p "next line test--------------------------------"
-build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 8])
-p "next line test--------------------------------"
-build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 8, 8])
+# p "next line test--------------------------------"
+# build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 8])
+# p "next line test--------------------------------"
+# build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 8, 8])
