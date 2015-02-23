d5ea1cf8efb9458d0772526538811e1495c659ff
Alex Hardt <hardt.alex@gmail.com>
2015-02-23 11:19:12 -0600

    using someones testing to help drive development

---
+++ second_binary_tree.rb
@@ -0,0 +1,9 @@
+#found someone else's testing on github, using it to help drive my devleopment:https://gist.github.com/yuya-takeyama/812489
+
+module BinaryTree
+  class Node
+    def share_examples_for(arg)
+
+    end
+  end
+end
