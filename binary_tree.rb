fb456bdb797925b39248ce8e4df3405f2dd3bde7
Alex Hardt <hardt.alex@gmail.com>
2015-02-24 10:29:53 -0600

    i want to see the actual tree, i can't see it . i want to be able to see the binary tree as it is built and then as it is deconstructed
    i found a stackoverflow article that addreses this problem: http://stackoverflow.com/questions/801740/c-how-to-draw-a-binary-tree-to-the-console

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,61 +1,3 @@
-#what am i doing?
-#input: arbitrary input
-#output: locate data inside of it?
-
-#don't i need: an array, a min, a max, and ______???(something else)
-
-#so i need to find a 'root node' then create a binary tree out of that information. it would be cool to print out each level of the tree on a new line
-
-#found someone else's testing on github, using it to help drive my devleopment:https://gist.github.com/yuya-takeyama/812489
-
-# module BinaryTree
-#   class Node
-#     def share_examples_for(arg)
-
-#     end
-#   end
-# end
-
-#idk what the above is, using below for me
-
-#so my node class accepts the array value, pulls out a parent node and children nodes
-
-#it seems like I'm trying to do a lot, lets break it down
-  # 1. take an array as an input for a new class
-  # 2. identify the midpoint aka get the parent node
-  # 3. find the children node of the parent nodes
-  #     3.1 the parent node will have two children arrays
-  #     3.2 the two children arrays will provide the child nodes and
-  #         those nodes will make the
-
-  #i want the parent node finder to return the parent node and the children arrays or children
-
-#so does the node class creat node objects, or create
-#im getting confused on when I'm accepting arrays to find my parent node and when i need to create a node object with the node
-
-# two ideas here:
-#1.  I accept an array to find my original parrent node
-#2. i accept a single number to create a node object
-
-# a new node is passed a single number value, that is its value
-  #but, it could have children, or a whole number of descendents.
-    #this is what I am having trouble acomadating
-
-    #if there is an array, then there is def a child
-    #if there is one number, there is a child
-    #if the array is empty, there is no child
-
-    #will take an array as input
-    #will return a child, or will say there is no child
-    #how should no child be represented (an empty array)
-
-    #i'm also trying to creat a basecase here where if the array is empty, you don't creat another node object
-
-    #what happens if I the node doesn't have a child, it just should exist, like the search can't go further
-
-    #really, the way my node should look is just show the value, and the two children after that, there will be nodes within that
-
-
 class Node
   attr_accessor :value, :left_child, :right_child
 
@@ -81,7 +23,8 @@ class BinarySearchTree
   def build_tree(array, left=0, right=array.length-1)
     return if left > right
 
-    array = merge_sort(array)
+    array = array.sort
+
 
     index_mid = left +(right-left)/2
     node = Node.new(array[index_mid])
@@ -90,76 +33,91 @@ class BinarySearchTree
     node.right_child = build_tree(array, index_mid+1, right)
     #i was really close in my build of this, the .left_child notation was a little tricky for me though. it is usable b/c of the
 
+    @tree = node
+    @tree
+
   end
 
-end
+  def breadth_first_search(query)
+    p "guess is #{query}"
+    queue = []
+    root = @tree.value
+    p "hey root is #{root}"
+
+    return @tree if root == query
+
+    left_child = @tree.left_child
+    right_child = @tree.right_child
+
+    queue << left_child if left_child != nil
+    queue << right_child if right_child != nil
+
+    p "hey before loop, queue is #{queue}"
+
+    count = 0
+    loop do
+      count +=1
+      return "queue is empty!!!" if queue.empty?
+      node = queue.shift
+      p "node is #{node.value}"
+
+      return node.value if query == node.value
 
 
-
-
-
-
-def left_child_finder(array)
-end
-def right_child_finder(array)
-end
-
-class Node
-  attr_reader :solo_value, :left_child, :right_child, :array
-
-  def initialize(array) #not sure there need to be optional args passed here
-
-    array = array.sort
-
-    @parent_node = #i have no idea
-
-    @solo_value = parent_node_finder(array)
-    @left_children_array = left_children_array_finder(array)
-    @right_children_array = right_children_array_finder(array)
-    @left_child = @left_children_array[-1]
-    @right_child = @right_children_array[0]
-
-    #the solo value here has to exist, but the other value of children doesn't have to exist
-  end
-
-  def parent_node_finder(array)
-    array[(array.length/2)]
-  end
-
-  def left_children_array_finder(array)
-    index_of_midpoint = array.index(@solo_value)
-    left_children = array[0..(index_of_midpoint-1)]
-  end
-
-  def right_children_array_finder(array)
-    index_of_midpoint = array.index(@solo_value)
-    right_child = array[(index_of_midpoint+1)..-1]
-  end
-
-
-  def build_tree(array)
-    if array.length == 0
-      p "we are at the last leaf, no more children to create node objects with"
-    elsif array.length == 1
-      p "elsif comment"
-      childless_node = Node.new(array[0])
-
-    else
-      p "else comment"
-      array = array.sort
-      length_of_array = array.length
-      midpoint = array[(length_of_array/2)]
-
-      index_of_midpoint = array.index(midpoint)
-      left_child = array[0..(index_of_midpoint-1)]
-      right_child = array[(index_of_midpoint+1)..-1]
-      original_node = Node.new(midpoint, left_child, right_child)
-      #i can't call the node method on one nubmer!!!
-      #i was making a call for the array, and then also for the value
+      queue << node.left_child if node.left_child != nil
+      queue << node.right_child if node.right_child != nil
+      p ""
+      p queue
+      p ""
+      p "queue length is: #{queue.length}"
+      p "Count is #{count}"
     end
   end
-end #ends node class
+
+  def depth_first_search(querry)
+    # stack =  #something?
+    #if it goes in stack, it then goes into result
+    #there is a criteria about once something has been visited, or if it will be visited...
+    # result = #something
+  end
+
+  def display_tree()#block to be passed
+    list = []
+    yield @tree.value
+    left_child = @tree.left_child
+    right_child = @tree.right_child
+
+    list << left_child if left_child != nil
+    list << right_child if right_child != nil
+
+    loop do
+      break if list.empty?
+      node = list.shift
+      yield node.value
+      list << node.left_child if node.left_child != nil
+      list << node.right_child if node.right_child != nil
+    end
+  end
+
+end #ends binary search tree class
+
+an_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+a_tree = BinarySearchTree.new(an_array)
+
+p a_tree.display_tree {|x| print " #{x} "}
 
 
-node_experiment = Node.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p node_experiment
+
+a_tree.breadth_first_search(7)
+p '---------'
+p '---------'
+a_tree.breadth_first_search(4)
+
+p '---------'
+p '---------'
+a_tree.breadth_first_search(6)
+
+
+
+
